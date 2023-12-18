a68938613b6579e1ba696f012c9cc11c5b55b7d7
Dylan Wallach <22645038+FireWallach@users.noreply.github.com>
2023-12-17 20:48:18 -0500

    Added Email Monitoring and Automatic Trigger

--- lib/email_to_split_bill-stack.ts
+++ lib/email_to_split_bill-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from "aws-cdk-lib";
 import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
+import * as iam from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 import { Function, Runtime, Code, LayerVersion } from "aws-cdk-lib/aws-lambda";
-import { LambdaRestApi, RequestValidator } from "aws-cdk-lib/aws-apigateway";
 import path = require("path");
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
 
 export class EmailToSplitBillStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,6 +14,11 @@ export class EmailToSplitBillStack extends cdk.Stack {
     // Declarations
     const splitwiseLayer = new LayerVersion(this, "SplitwiseLayer", {
       code: Code.fromAsset(path.join(__dirname, "/lambda-layer")),
+      compatibleRuntimes: [Runtime.NODEJS_16_X],
+    });
+
+    const emailLayer = new LayerVersion(this, "EmailLayer", {
+      code: Code.fromAsset(path.join(__dirname, "email-layer")),
       compatibleRuntimes: [Runtime.NODEJS_16_X],
     });
 
@@ -28,30 +35,41 @@ export class EmailToSplitBillStack extends cdk.Stack {
       code: Code.fromAsset(path.join(__dirname, "../deploy")),
       handler: "handleEmail.handler",
       layers: [splitwiseLayer],
+      timeout: cdk.Duration.seconds(30),
       environment: {
         SPLITWISE_SECRET_ARN: splitwiseSecret.secretArn,
       },
     });
 
-    const handleEmailAPI: LambdaRestApi = new LambdaRestApi(
-      this,
-      "EmailApiGateway",
-      {
-        handler: handleEmailLambda,
-        proxy: false,
-      }
-    );
-
-    const validator = new RequestValidator(this, "RequestValidator", {
-      restApi: handleEmailAPI,
-      validateRequestBody: true,
+    const checkForBillLambda: Function = new Function(this, "checkForBill", {
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset(path.join(__dirname, "../deploy")),
+      handler: "checkForBill.handler",
+      layers: [emailLayer],
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 256,
+      environment: {
+        SPLITWISE_SECRET_ARN: splitwiseSecret.secretArn,
+        HANDLE_EMAIL_LAMBDA_ARN: handleEmailLambda.functionArn,
+      },
     });
 
-    // Configuration
-    handleEmailAPI.root.addMethod("POST", undefined, {
-      requestValidator: validator,
-    });
-
+    // Grant Permissions
     splitwiseSecret.grantRead(handleEmailLambda);
+    splitwiseSecret.grantRead(checkForBillLambda);
+
+    const lambdaInvokedPolicy = new iam.PolicyStatement({
+      actions: ["lambda:InvokeFunction"],
+      resources: [handleEmailLambda.functionArn],
+    });
+
+    checkForBillLambda.addToRolePolicy(lambdaInvokedPolicy);
+
+    // Schedule
+    const checkEmailRule = new events.Rule(this, "Rule", {
+      schedule: events.Schedule.rate(cdk.Duration.minutes(10)),
+    });
+
+    checkEmailRule.addTarget(new targets.LambdaFunction(checkForBillLambda));
   }
 }
